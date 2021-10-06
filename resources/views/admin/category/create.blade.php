c48137f6e862d591d14e87c51dc65c0a5cab664b
Nguyen Kiet <nguyenkiet0807@gmail.com>
2021-10-06 16:16:11 +0700

    init category

---
+++ resources/views/admin/category/create.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Category Manager')
+
+@section('content_header')
+    <h1>Create category</h1>
+@stop
+
+@section('content')
+    <p class="text-primary">Welcome to this beautiful admin panel.</p>
+@stop
