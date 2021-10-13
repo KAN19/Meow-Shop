c0d03a9234598b84db7a64d46bc48393d3cce9b5
Nguyen Kiet <nguyenkiet0807@gmail.com>
2021-10-13 09:53:55 +0700

    config menu

--- config/adminlte.php
+++ config/adminlte.php
@@ -225,99 +225,64 @@ return [
     */
 
     'menu' => [
-        // Navbar items:
-        [
-            'type'         => 'navbar-search',
-            'text'         => 'search',
-            'topnav_right' => true,
-        ],
-        [
-            'type'         => 'fullscreen-widget',
-            'topnav_right' => true,
-        ],
 
         // Sidebar items:
+        ['header' => 'POSTS'],
         [
-            'type' => 'sidebar-menu-search',
-            'text' => 'search',
-        ],
-        [
-            'text' => 'blog',
-            'url'  => 'admin/blog',
-            'can'  => 'manage-blog',
-        ],
-        [
-            'text'        => 'pages',
-            'url'         => 'admin/pages',
-            'icon'        => 'far fa-fw fa-file',
+            'text'        => 'All Posts',
+            'url'         => 'admin/#',
+            'icon'        => 'far fa-paper-plane',
             'label'       => 4,
             'label_color' => 'success',
         ],
-        ['header' => 'account_settings'],
         [
-            'text' => 'profile',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-user',
+            'text'        => 'Add new post',
+            'url'         => 'admin/#',
+            'icon'        => 'fas fa-plus-circle',
+           
         ],
+        
+        ['header' => 'PRODUCTS'],
         [
-            'text' => 'change_password',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-lock',
-        ],
-        [
-            'text'    => 'multilevel',
-            'icon'    => 'fas fa-fw fa-share',
+            'text'    => 'Product category',
+            'icon' => 'fas fa-stream',
             'submenu' => [
                 [
-                    'text' => 'level_one',
-                    'url'  => '#',
+                    'text' => 'All product categories',
+                    'route' => 'show-category',
                 ],
                 [
-                    'text'    => 'level_one',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'level_two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
+                    'text' => 'Add new category',
+                    'route' => 'create-category',
+                    'icon' => 'fas fa-plus-circle',
+
                 ],
             ],
         ],
-        ['header' => 'labels'],
         [
-            'text'       => 'important',
-            'icon_color' => 'red',
-            'url'        => '#',
+            'text'    => 'Product',
+            'icon'    => 'fas fa-shopping-cart',
+            'submenu' => [
+                [
+                    'text' => 'All products',
+                    'url'  => '#',
+                ],
+                [
+                    'text' => 'Add new product',
+                    'url'  => '#',
+                    'icon' => 'fas fa-plus-circle',
+                ],
+            ],
         ],
+        ['header' => 'ORDERS'],
         [
-            'text'       => 'warning',
-            'icon_color' => 'yellow',
-            'url'        => '#',
+            'text' => 'All orders',
+            'url'  => 'admin/#',
+            'icon' => 'fas fa-file-alt',
+            'label'       => 4,
+            'label_color' => 'success',
         ],
-        [
-            'text'       => 'information',
-            'icon_color' => 'cyan',
-            'url'        => '#',
-        ],
+
     ],
 
     /*
