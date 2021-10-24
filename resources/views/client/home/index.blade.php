6c0a8fa26061ac08b765af68c734c4682c632fb6
mthu153 <mthu153vt@gmail.com>
2021-10-24 22:30:20 +0700

    test

--- resources/views/client/home/index.blade.php
+++ resources/views/client/home/index.blade.php
@@ -1,17 +1,18 @@
 @extends('client.master')
 
 @section('style')
-    <link rel="stylesheet" href="/css/app.css">
+<link rel="stylesheet" href="/css/app.css">
 @endsection
 
 @section('title', 'title ne')
-    
+
 @section('jquery')
-    <script src="/js/home.js"></script>
+<script src="/js/home.js"></script>
 @endsection
 
 @section('content')
-    <h1>
-        Home page
-    </h1>
+<h1>
+    Home page
+    Category
+</h1>
 @endsection
