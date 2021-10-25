ead7b3c0b4764b4730267011494a7fbf7d487aa1
mthu153 <mthu153vt@gmail.com>
2021-10-25 09:22:09 +0700

    slide

--- resources/views/client/home/index.blade.php
+++ resources/views/client/home/index.blade.php
@@ -4,15 +4,50 @@
 <link rel="stylesheet" href="/css/app.css">
 @endsection
 
-@section('title', 'title ne')
+@section('title', 'HomePage')
 
 @section('jquery')
 <script src="/js/home.js"></script>
 @endsection
 
+@section('javascript')
+<script type="text/javascript" href="/js/home.js"> </script>
+@endsection
+
 @section('content')
-<h1>
-    Home page
-    Category
-</h1>
+
+<div class="slideshow-container">
+
+    <!-- Full-width images with number and caption text -->
+    <div class="mySlides fade">
+        <div class="numbertext">1 / 3</div>
+        <img src="public\filemanager\img\slide1.png" style="width:100%">
+        <div class="text">Caption Text</div>
+    </div>
+
+    <div class="mySlides fade">
+        <div class="numbertext">2 / 3</div>
+        <img src="public\filemanager\img\slide2.png" style="width:100%">
+        <div class="text">Caption Two</div>
+    </div>
+
+    <div class="mySlides fade">
+        <div class="numbertext">3 / 3</div>
+        <img src="public\filemanager\img\slide3.png" style="width:100%">
+        <div class="text">Caption Three</div>
+    </div>
+
+    <!-- Next and previous buttons -->
+    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+<!-- The dots/circles -->
+<div style="text-align:center">
+    <span class="dot" onclick="currentSlide(1)"></span>
+    <span class="dot" onclick="currentSlide(2)"></span>
+    <span class="dot" onclick="currentSlide(3)"></span>
+</div>
+
 @endsection
