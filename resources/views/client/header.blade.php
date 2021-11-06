9933d0fdaae274e940b4388472de185e641f19e0
Nguyen Kiet <nguyenkiet0807@gmail.com>
2021-11-06 11:39:42 +0700

    add backdrop + nav

--- resources/views/client/header.blade.php
+++ resources/views/client/header.blade.php
@@ -1,1 +1,23 @@
-Header day
+<nav class="navbar__container">
+    <div class="navbar__topnav">
+        <div class="topnav__item">
+            This is lgo
+        </div>
+
+        <div class="topnav__item topnav__item-burger" id="burger-top">
+            <i class="fas fa-bars"></i>
+        </div>
+
+    </div>
+    <div class="sidenav">
+        <div class="sidenav__container">
+            <div class="sidenav__container__item">
+                Nav item
+            </div>
+            <div class="sidenav__container__item">
+                Nav item
+            </div>
+        </div>
+    </div>
+</nav>
+<div class="modal-overlay" id="modal-overlay"></div>
