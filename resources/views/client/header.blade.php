6eac1ce17a65aa87754e0ce98e00d6d0ee84bc1b
Nguyen Kiet <nguyenkiet0807@gmail.com>
2021-11-07 10:37:02 +0700

    reponsive navbar bien mat

--- resources/views/client/header.blade.php
+++ resources/views/client/header.blade.php
@@ -1,26 +1,59 @@
 <nav class="navbar__container">
     <div class="navbar__topnav">
-        <div class="topnav__item">
+        <div class="topnav__item ">
             This is lgo
         </div>
 
-        <div class="topnav__item topnav__item-burger" id="burger-top">
-            <i class="fas fa-bars"></i>
+        <div class="topnav__item topnav__item-fullscreen">
+            <a href="#" class="topnav__item__button">
+                Home
+            </a>
+            <a href="#" class="topnav__item__button">
+                Product
+            </a>
+            <a href="#" class="topnav__item__button">
+                Contact
+            </a>
+
         </div>
 
+        <div class="topnav__item topnav__item-fullscreen">
+            <div class="topnav__item__button">
+                Search
+            </div>
+            <div class="topnav__item__button">
+                Cart
+            </div>
+            <div class="topnav__item__button">
+                Profile
+            </div>
+        </div>
+
+        <!-- Burger -->
+        <div class="topnav__item topnav__item-burger d-flex d-sm-none" id="burger-top">
+            <i class="fas fa-bars"></i>
+        </div>
     </div>
     <div class="sidenav ">
-        <div class="sidenav__topnav" id="sidenav__burger">
-            <i class="fas fa-bars"></i>
+        <div class="sidenav__topnav">
+            <div class="topnav__item-burger">
+                <i class="fas fa-bars" id="sidenav__burger"></i>
+            </div>
         </div>
 
         <div class="sidenav__container">
-            <div class="sidenav__container__item">
-                Nav item
-            </div>
-            <div class="sidenav__container__item">
-                Nav item
-            </div>
+            <a href="#" class="sidenav__container__item">
+                Home
+            </a>
+            <a href="#" class="sidenav__container__item">
+                Product
+            </a>
+            <a href="#" class="sidenav__container__item">
+                Contact
+            </a>
+            <a href="#" class="sidenav__container__item">
+                Shopping cart
+            </a>
         </div>
     </div>
 </nav>
