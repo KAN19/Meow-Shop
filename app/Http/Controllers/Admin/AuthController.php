c1306b8aedd98f9980f9ed04320c7788a55a5d47
Nguyen Kiet <nguyenkiet0807@gmail.com>
2021-11-17 15:26:47 +0700

    login admin ok

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
+use App\Models\admin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -14,7 +14,7 @@ class AuthController extends Controller
     } 
 
     public function storeRegister(Request $request) {
-        $user = new User(); 
+        $user = new admin(); 
         $user->name = $request->username;
         $user->email = $request->email;
         $user->password = bcrypt($request->password);
@@ -29,11 +29,20 @@ class AuthController extends Controller
     } 
 
     public function login(Request $request) {
-        if (Auth::attempt(['name' => $request->username, 'password' => $request->password])) {
-            echo 'login ok'; 
+
+        $auth = Auth::guard('admin');
+
+        if ($auth instanceof \Illuminate\Contracts\Auth\StatefulGuard){
+            
+            $auth->attempt(['name' => $request->username, 'password' => $request->password]);
+            return redirect()->route('admin-home');
+
         } else {
-            echo 'login ko thanh cong'; 
+
+            return redirect()->route('admin-login')->with('Fail', 'Incorrect Credentials');
+       
         }
+     
     } 
 
     public function logout()
--- routes/web.php
+++ routes/web.php
@@ -36,67 +36,63 @@ Route::get('/test-admin', function () {
 
 
 
-Route::get('/admin-register', [AuthController::class, 'showRegister'])->name('admin-register');
-Route::post('/admin-register', [AuthController::class, 'storeRegister'])->name('admin-register');
 
+Route::middleware(['guest:admin'])->group(function() { 
 
-Route::get('/admin-login', [AuthController::class, 'showLogin'])->name('admin-login');
-Route::post('/admin-login', [AuthController::class, 'login'])->name('admin-login');
+    // Route::get('/admin-register', [AuthController::class, 'showRegister'])->name('admin-register');
+    // Route::post('/admin-register', [AuthController::class, 'storeRegister'])->name('admin-register');
+    
+    
+    Route::get('/admin-login', [AuthController::class, 'showLogin'])->name('admin-login');
+    Route::post('/admin-login', [AuthController::class, 'login'])->name('admin-login');
+    
+    
+});
+Route::middleware(['auth:admin'])->group(function() {
+    Route::get('/admin-logout', [AuthController::class, 'logout'])->name('admin-logout');
+    
 
-Route::get('/admin-logout', [AuthController::class, 'logout'])->name('admin-logout');
+    Route::prefix('admin')->group(function() {
 
-
-Route::prefix('admin')->group(function() {
-
-    Route::get('/', function () {
-        return view('admin.dashboard.index');
-    })->name('admin-home');
-
-    Route::prefix('category')->group(function() {
-        Route::get('', [CategoryController::class, 'showCategory'])->name('show-category');
-
-        Route::get('create', [CategoryController::class, 'showCreateCategory'])->name('create-category');
-        Route::post('create', [CategoryController::class, 'storeCategory']);
-
-        Route::get('/{slug}', [CategoryController::class, 'showEditCategory'])->name('edit-category');
-        Route::put('/{slug}', [CategoryController::class, 'updateCategory']); 
-
-        Route::get('delete/{slug}', [CategoryController::class, 'deleteCategory'])->name('delete-category');
-
+        Route::get('/', function () {
+            return view('admin.dashboard.index');
+        })->name('admin-home');
+    
+        Route::prefix('category')->group(function() {
+            Route::get('', [CategoryController::class, 'showCategory'])->name('show-category');
+    
+            Route::get('create', [CategoryController::class, 'showCreateCategory'])->name('create-category');
+            Route::post('create', [CategoryController::class, 'storeCategory']);
+    
+            Route::get('/{slug}', [CategoryController::class, 'showEditCategory'])->name('edit-category');
+            Route::put('/{slug}', [CategoryController::class, 'updateCategory']); 
+    
+            Route::get('delete/{slug}', [CategoryController::class, 'deleteCategory'])->name('delete-category');
+    
+        });
+    
+        Route::prefix('product')->group(function() {
+            Route::get('', [ProductController::class, 'showProduct'])->name('show-product');
+            
+            Route::get('create', [ProductController::class, 'showCreateProduct'])->name('create-product');
+            Route::post('create', [ProductController::class, 'storeProduct']);
+    
+            Route::get('/{slug}', [ProductController::class, 'showEditProduct'])->name('edit-product');
+            Route::put('/{slug}', [ProductController::class, 'updateProduct']); 
+    
+            // Route::get('delete/{id}', [CategoryController::class, 'deleteCategory'])->name('delete-category');
+    
+        });
+    
+        Route::prefix('media')->group(function() {
+            Route::get('', [MediaController::class, 'index'])->name('show-media');
+            
+        });
+       
+    
+        // Route::get('/media2', function () {
+        //     return view('admin.media.index2');
+        // });
     });
-
-    Route::prefix('product')->group(function() {
-        Route::get('', [ProductController::class, 'showProduct'])->name('show-product');
-        
-        Route::get('create', [ProductController::class, 'showCreateProduct'])->name('create-product');
-        Route::post('create', [ProductController::class, 'storeProduct']);
-
-        Route::get('/{slug}', [ProductController::class, 'showEditProduct'])->name('edit-product');
-        Route::put('/{slug}', [ProductController::class, 'updateProduct']); 
-
-        // Route::get('delete/{id}', [CategoryController::class, 'deleteCategory'])->name('delete-category');
-
-    });
-
-    Route::prefix('media')->group(function() {
-        Route::get('', [MediaController::class, 'index'])->name('show-media');
-        
-        // Route::post('/upload', [MediaController::class, 'storeMedia'])->name('upload-media');
-
-        // Route::get('/{id}', [MediaController::class, 'showEditMedia'])->name('edit-media');
-
-        // Route::get('create', [CategoryController::class, 'showCreateCategory'])->name('create-category');
-        // Route::post('create', [CategoryController::class, 'storeCategory']);
-
-        // Route::get('edit/{id}', [CategoryController::class, 'showEditCategory'])->name('edit-category');
-        // Route::put('edit/{id}', [CategoryController::class, 'updateCategory']); 
-
-        // Route::get('delete/{id}', [CategoryController::class, 'deleteCategory'])->name('delete-category');
-
-    });
-
-
-    // Route::get('/media2', function () {
-    //     return view('admin.media.index2');
-    // });
+    
 });
