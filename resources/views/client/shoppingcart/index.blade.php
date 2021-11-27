bf74984283b5ccc7a7237fb5b5140310819a85e9
Nguyen Kiet <nguyenkiet0807@gmail.com>
2021-11-27 09:50:09 +0700

    NgKiet refactor code

--- resources/views/client/shoppingcart/index.blade.php
+++ resources/views/client/shoppingcart/index.blade.php
@@ -10,188 +10,112 @@
 
 @section('content')
 <div class=".container-fluid">
+    <!-- <div class="banner"> -->
     <div class="banner">
-        <div class="banner__name">
-            <div class="banner__name--size">CART</div>
-        </div>
-        <!-- thanh dieu huong -->  
-        <nav aria-label="breadcrumb">
-            <ol class="breadcrumb" >
-                <li class="breadcrumb-item ml-5"><a href="#"><i class="fa fa-home" aria-hidden="true"></i></a></li>
-                <li class="breadcrumb-item"><a href="#">Cart</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Shop</li>
-            </ol>
-        </nav>         
-    </div> 
-    <h1 class="text-center mb-5">YOUR CART ITEMS</h1>
+        <div class="banner__title">CART</div>
+    </div>
+    <!-- thanh dieu huong -->
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item ml-5"><a href="#"><i class="fa fa-home" aria-hidden="true"></i></a></li>
+            <li class="breadcrumb-item"><a href="#">Cart</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Shop</li>
+        </ol>
+    </nav>
+    <!-- </div> -->
+    <h3 class="text-center mb-5">YOUR CART ITEMS</h3>
     <!-- tieu de -->
-    <div class="cart row p-3 mb-2">
-        <div class="cart__delete col-md-1">Delete</div> 
-        <div class="cart__product col-md-3">
-            Product
+    <div class="cart__container">
+        <div class="cart row p-3 mb-2">
+            <div class="cart__title  col-md-2">
+                Image
+            </div>
+            <div class="cart__title  col-md-3">
+                Name
+            </div>
+            <div class="cart__title col-md-2">
+                Price
+            </div>
+            <div class="cart__title col-md-2">
+                Quantity
+            </div>
+            <div class="cart__title col-md-2">
+                Subtotal
+            </div>
+            <div class="cart__title col-md-1">Delete</div>
         </div>
-        <div class="cart__name col-md-2">
-            Name
+
+
+        <div class="row ">
+            <div class="product__set col-md-2">
+                <img class="product__set--image" id="image" src="https://picsum.photos/300/130" />
+            </div>
+            <div class="product__name col-md-3">
+                <a class="product--name">Product Name</a>
+            </div>
+            <div class="product__price col-md-2">
+                <del>9.99$</del> <strong>8.99$</strong>
+            </div>
+            <!-- quanlity -->
+            <div class="product__quanlity col-md-2">
+                <div class="buttons_added">
+                    <input class="minus is-form" type="button" value="-">
+                    <input aria-label="quantity" class="input-qty" max="10" min="1" name="" type="number" value="1">
+                    <input class="plus is-form" type="button" value="+">
+                </div>
+            </div>
+            <div class="product__subtotal col-md-2">
+                $9.99
+            </div>
+            <!-- icon bin -->
+            <div class="btn__bin col-md-1">
+                <i class="bin--icon fa fa-trash" aria-hidden="true"></i>
+            </div>
         </div>
-        <div class="cart__price col-md-2">
-            Price
-        </div>
-        <div class="cart__quanlity col-md-2">
-            Quanlity
-        </div>
-        <div class="cart__subtotal col-md-1">
-            Subtotal
-        </div>
-        <div class="col-md-1"></div>       
-    </div>
-    <!-- san pham -->
-    <!-- san pham 1 -->
-    <div class="row product">
-        <div class="col-md-1 checkbox__delete">
-            <i class="container">
-                <!-- checkbox delete -->
-                <input type="checkbox" id="c2" value=""><label class="label__checkbox" for="c2"><span class="span__checkbox"></span></label>
-            </i>
-        </div>
-        <div class="product__set col-md-3">            
-            <img class="product__set--image" id="image" src="https://picsum.photos/180/130"/>
-        </div>
-        <div class="product__name col-md-2">
-            <a class="product--name">Product Name</a>
-        </div>
-        <div class="product__price col-md-2">
-            <del>9.99$</del> <strong>8.99$</strong>
-        </div>
-        <!-- quanlity -->
-        <div class="product__quanlity col-md-2">
-            <div class="buttons_added">
-                <input class="minus is-form" type="button" value="-">
-                <input aria-label="quantity" class="input-qty" max="10" min="1" name="" type="number" value="1">
-                <input class="plus is-form" type="button" value="+">
-            </div>            
-        </div>
-        <div class="product__subtotal col-md-1">
-            $9.99
-        </div>       
-        <!-- icon bin -->
-        <div class="btn__bin col-md-1">
-            <i class="bin--icon fa fa-trash" aria-hidden="true"></i>
-        </div>
-    </div>    
-    <hr class="line my-3">   
-    <!-- san pham 2 -->
-    <div class="row product">
-        <div class="col-md-1 checkbox__delete">
-            <i class="container">
-                <!-- checkbox delete -->
-                <input type="checkbox" id="c2" value=""><label class="label__checkbox" for="c2"><span class="span__checkbox"></span></label>
-            </i>
-        </div>
-        <div class="product__set col-md-3">            
-            <img class="product__set--image" id="image" src="https://picsum.photos/180/130"/>
-        </div>
-        <div class="product__name col-md-2">
-            <a class="product--name">Product Name</a>
-        </div>
-        <div class="product__price col-md-2">
-            <del>9.99$</del> <strong>8.99$</strong>
-        </div>
-        <!-- quanlity -->
-        <div class="product__quanlity col-md-2">
-            <div class="buttons_added">
-                <input class="minus is-form" type="button" value="-">
-                <input aria-label="quantity" class="input-qty" max="10" min="1" name="" type="number" value="1">
-                <input class="plus is-form" type="button" value="+">
-            </div>            
-        </div>
-        <div class="product__subtotal col-md-1">
-            $9.99
-        </div>       
-        <!-- icon bin -->
-        <div class="btn__bin col-md-1">
-            <i class="bin--icon fa fa-trash" aria-hidden="true"></i>
-        </div>
-    </div>    
-    <hr class="line my-3">   
-    <!-- san pham 3 -->
-    <div class="row product">
-        <div class="col-md-1 checkbox__delete">
-            <i class="container">
-                <!-- checkbox delete -->
-                <input type="checkbox" id="c2" value=""><label class="label__checkbox" for="c2"><span class="span__checkbox"></span></label>
-            </i>
-        </div>
-        <div class="product__set col-md-3">            
-            <img class="product__set--image" id="image" src="https://picsum.photos/180/130"/>
-        </div>
-        <div class="product__name col-md-2">
-            <a class="product--name">Product Name</a>
-        </div>
-        <div class="product__price col-md-2">
-            <del>9.99$</del> <strong>8.99$</strong>
-        </div>
-        <!-- quanlity -->
-        <div class="product__quanlity col-md-2">
-            <div class="buttons_added">
-                <input class="minus is-form" type="button" value="-">
-                <input aria-label="quantity" class="input-qty" max="10" min="1" name="" type="number" value="1">
-                <input class="plus is-form" type="button" value="+">
-            </div>            
-        </div>
-        <div class="product__subtotal col-md-1">
-            $9.99
-        </div>       
-        <!-- icon bin -->
-        <div class="btn__bin col-md-1">
-            <i class="bin--icon fa fa-trash" aria-hidden="true"></i>
-        </div>
-    </div>    
-    <!-- btn continue shopping va clear carg -->
-    <hr class="line my-3 ">s
-    <div class="row btn__cart">
-        <div class="col-md">
-            <div class="container overlay__btn">
+        <hr class="line my-3 ">
+        <div class="row btn__cart ">
+            <div class="overlay__btn">
                 <button type="button" class="btn continue--btn">Continue Shopping</button>
-                <!-- <i class="overlay overlay__btn--continue fa fa-shopping-cart" aria-hidden="true"></i> -->
-            </div>   
-        </div>     
-        <div class="col-md btn__cart--clear">
-            <button type="button" class="btn clear--btn">Clear Shopping Cart</button>
+            </div>
+            <div class="overlay__btn">
+                <button type="button" class="btn clear--btn">Clear Shopping Cart</button>
+            </div>
+        </div>
+        <!--  table cart total -->
+        <hr class="line my-3 ">
+        <div class="container-fluid">
+            <p class="carttotal">Cart Total</p>
+            <table class="table table-bordered">
+                <tbody>
+                    <tr>
+                        <th>Subtotal</th>
+                        <td>$35.98</td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <th>Shipping</th>
+                        <td>COD - Cash On Delivery</td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <th>Total</th>
+                        <td>$35.98</td>
+                    </tr>
+                </tbody>
+            </table>
+            <!-- btn checkout -->
+            <div class="btn__cart">
+                <div class="overlay__btn">
+                    <button type="button" class="btn checkout--btn">Procceed to Checkout</button>
+                </div>
+            </div>
         </div>
     </div>
-   <!--  table cart total -->
-    <hr class="line my-3 ">
-    <div class="container-fluid">
-        <p class="carttotal">Cart Total</p>
-        <table class="table table-bordered">
-            <tbody>
-                <tr>
-                    <th>Subtotal</th>
-                    <td>$35.98</td>
-                </tr>
-            </tbody>
-            <tbody>
-                <tr>
-                    <th>Shipping</th>
-                    <td>COD - Cash On Delivery</td>
-                </tr>
-            </tbody>
-            <tbody>
-                <tr>
-                    <th>Total</th>
-                    <td>$35.98</td>
-                </tr>
-            </tbody>
-        </table>
-        <!-- btn checkout -->
-        <div class="checkout">
-             <button type="button" class="btn checkout--btn">Procceed to Checkout</button>
-        </div>       
-    </div>
-  
 </div>
-    
-    
+
+
 @endsection
 
 @section('javascript')
