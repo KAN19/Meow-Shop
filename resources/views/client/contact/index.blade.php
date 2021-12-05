6c7596985fc4533efeb0bad9cdc877803a4f9b19
Nguyen Kiet <nguyenkiet0807@gmail.com>
2021-12-05 11:17:44 +0700

    nguyen kiet refactor code oke

--- resources/views/client/contact/index.blade.php
+++ resources/views/client/contact/index.blade.php
@@ -14,7 +14,6 @@
     <div class="banner">
         <div class="banner__name">
             <div class="banner__name--title">CONTACT</div>
-            <div>Home - Contact</div>
         </div>
         <!-- form -->
         <div class="container">
@@ -38,28 +37,15 @@
                 </div>
                 <div class="row">
                     <div class="col-25">
-                        <label for="province">Province</label>
-                    </div>
-                    <div class="col-75">
-                        <select id="country" name="country">
-                            <option value="hcm">Hồ Chí Minh</option>
-                            <option value="hanoi">Hà Nội</option>
-                            <option value="vungtau">Vũng Tàu</option>
-                            <option value="vinh">Vinh</option>
-                            <option value="thanhhoa">Thanh Hóa</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-25">
                         <label for="subject">Message</label>
                     </div>
                     <div class="col-75">
-                        <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+                        <textarea id="subject" name="subject" placeholder="Write something.."
+                            style="height:200px"></textarea>
                     </div>
                 </div>
                 <div class="submit">
-                    <button type="button" class="btn--submit">Submit</button>
+                    <input type="submit" value="Submit" class="btn--submit"></button>
                 </div>
             </form>
         </div>
