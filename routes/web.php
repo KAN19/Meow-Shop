9ab9a34660b24c95cd349f366a6fa6b599f1fdd2
mthu153 <mthu153vt@gmail.com>
2021-12-17 20:03:19 +0700

    them trang 'successful order

--- routes/web.php
+++ routes/web.php
@@ -32,10 +32,13 @@ Route::get('/products', function () {
     return view('client.products.index');
 })->name('product-page');
 
-Route::get('/products',[ClientProductController::class, "showProducts"])->name('product-page');
-Route::get('/product/{slug}',[ClientProductController::class, "showProductDetail"])->name('product-detail-page');
+Route::get('/products', [ClientProductController::class, "showProducts"])->name('product-page');
+Route::get('/product/{slug}', [ClientProductController::class, "showProductDetail"])->name('product-detail-page');
 
-
+// ========== Successful Order Notification route ========
+Route::get('/successfulorder', function () {
+    return view('client.successfulOrder.index');
+})->name('successful-order-page');
 
 // ========== Contact route ========
 Route::get('/contact', function () {
@@ -44,12 +47,12 @@ Route::get('/contact', function () {
 
 
 // ========== Cart Route ========
-Route::prefix('cart')->group(function() {
-    Route::get('/', [CartController::class, 'index'])->name('show-cart'); 
-    Route::get('add/{id}', [CartController::class, 'add'])->name('add-cart'); 
-    Route::get('remove/{id}', [CartController::class, 'remove'])->name('remove-cart'); 
-    Route::get('update/{id}', [CartController::class, 'update'])->name('update-cart'); 
-    Route::get('clear', [CartController::class, 'clear'])->name('clear-cart'); 
+Route::prefix('cart')->group(function () {
+    Route::get('/', [CartController::class, 'index'])->name('show-cart');
+    Route::get('add/{id}', [CartController::class, 'add'])->name('add-cart');
+    Route::get('remove/{id}', [CartController::class, 'remove'])->name('remove-cart');
+    Route::get('update/{id}', [CartController::class, 'update'])->name('update-cart');
+    Route::get('clear', [CartController::class, 'clear'])->name('clear-cart');
 });
 
 
@@ -60,74 +63,67 @@ Route::get('/resultsearch', function () {
 
 
 // ========== Checkout Route ========
-Route::prefix('/checkout')->group(function() {
+Route::prefix('/checkout')->group(function () {
     Route::get('', [CheckoutController::class, 'index'])->name('show-checkout');
     Route::post('', [CheckoutController::class, 'store'])->name('store-checkout');
-    
-
 });
 Route::get('test-mail', [CheckoutController::class, 'testMail'])->name('test-mail');
 
 
 //========= Admin routes ============
-Route::middleware(['guest:admin'])->group(function() { 
+Route::middleware(['guest:admin'])->group(function () {
 
     Route::get('/admin-register', [AuthController::class, 'showRegister'])->name('admin-register');
     Route::post('/admin-register', [AuthController::class, 'storeRegister'])->name('admin-register');
-    
-    
+
+
     Route::get('/admin-login', [AuthController::class, 'showLogin'])->name('admin-login');
     Route::post('/admin-login', [AuthController::class, 'login'])->name('admin-login');
-    
-    
 });
-Route::middleware(['auth:admin'])->group(function() {
+Route::middleware(['auth:admin'])->group(function () {
     Route::get('/admin-logout', [AuthController::class, 'logout'])->name('admin-logout');
-    
 
-    Route::prefix('admin')->group(function() {
+
+    Route::prefix('admin')->group(function () {
 
         Route::get('/', function () {
             return view('admin.dashboard.index');
         })->name('admin-home');
-    
-        Route::prefix('category')->group(function() {
+
+        Route::prefix('category')->group(function () {
             Route::get('', [CategoryController::class, 'showCategory'])->name('show-category');
-    
+
             Route::get('create', [CategoryController::class, 'showCreateCategory'])->name('create-category');
             Route::post('create', [CategoryController::class, 'storeCategory']);
-    
+
             Route::get('/{slug}', [CategoryController::class, 'showEditCategory'])->name('edit-category');
-            Route::put('/{slug}', [CategoryController::class, 'updateCategory']); 
-    
+            Route::put('/{slug}', [CategoryController::class, 'updateCategory']);
+
             Route::get('delete/{slug}', [CategoryController::class, 'deleteCategory'])->name('delete-category');
-    
         });
-    
-        Route::prefix('product')->group(function() {
+
+        Route::prefix('product')->group(function () {
             Route::get('', [ProductController::class, 'showProduct'])->name('show-product');
-            
+
             Route::get('create', [ProductController::class, 'showCreateProduct'])->name('create-product');
             Route::post('create', [ProductController::class, 'storeProduct']);
-    
+
             Route::get('/{slug}', [ProductController::class, 'showEditProduct'])->name('edit-product');
-            Route::put('/{slug}', [ProductController::class, 'updateProduct']); 
-    
+            Route::put('/{slug}', [ProductController::class, 'updateProduct']);
+
             // Route::get('delete/{id}', [CategoryController::class, 'deleteCategory'])->name('delete-category');
-    
+
         });
-    
-        Route::prefix('media')->group(function() {
+
+        Route::prefix('media')->group(function () {
             Route::get('', [MediaController::class, 'index'])->name('show-media');
-            
         });
-       
-    
+
+
         Route::get('/orders', [OrderController::class, 'index'])->name('show-orders');
         Route::get('/order/{id}', [OrderController::class, 'showOrderDetail'])->name('show-order-detail');
         Route::post('/order/confirm/{id}', [OrderController::class, 'confirmOrderDetail'])->name('confirm-order-detail');
         Route::post('/order/cancel/{id}', [OrderController::class, 'cancelOrderDetail'])->name('cancel-order-detail');
         Route::post('/order/complete/{id}', [OrderController::class, 'completeOrderDetail'])->name('complete-order-detail');
     });
-    
 });
