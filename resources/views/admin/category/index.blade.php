3e304cedf0e2ea087697015e43fd86260d3c9e9f
nhatkhue141 <nhatkhuetran142@gmail.com>
2021-12-18 20:32:49 +0700

    update category admin v1

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -4,6 +4,12 @@
 
 @section('content_header')
 <h1>List categories</h1>
+
+@stop
+
+
+@section('css')
+<link rel="stylesheet" href="/css/admin/category.css">
 @stop
 
 @section('breadcrumb')
@@ -12,19 +18,19 @@
 
 
 @section('content')
-<div id="page-wrapper">
+<div id="page-wrapper" class="category_admin">
     <div class="container-fluid">
         <div>
-            <a href="{{route('create-category')}}" class="btn btn-primary">Thêm mới</a>
+            <a href="{{route('create-category')}}" class="btn btn-primary btn_addcategory_admin">Thêm mới</a>
         </div>
 
         <div class="row">
             <!-- /.col-lg-12 -->
-            <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+            <table class="table table-striped table-bordered table-hover table_category" id="dataTables-example">
                 <thead>
                     <tr align="center">
                         <th>ID</th>
-                        <th>Name</th>
+                        <th style="width:40%">Name</th>
                         <th>Delete</th>
                         <th>Edit</th>
                     </tr>
@@ -34,10 +40,12 @@
                     <tr class="odd gradeX" align="center">
                         <td>{{$counter++}}</td>
                         <td>{{$item->name}}</td>
-                        <td class="center"><i class="fas fa-trash"></i><a
-                                href="{{route('delete-category', $item->slug)}}"> Delete</a></td>
-                        <td class="center"><i class="fas fa-pencil-alt"></i> <a
-                                href="{{route('edit-category', $item->slug)}}">Edit</a></td>
+                        <td class="center"><div class="btn btn-danger" id="btn_del_category"><i class="fas fa-trash"></i><a class="text_del"
+                                href="{{route('delete-category', $item->slug)}}"> Delete</a></div>
+                        </td>
+                        <td class="center"><div class="btn btn-info"id="btn_edit_category"><i class="fas fa-pencil-alt icon_edit_category"></i> <a class="text_edit"
+                                href="{{route('edit-category', $item->slug)}}">Edit</a></div>
+                        </td>
                     </tr>
                 </tbody>
                 @endforeach
